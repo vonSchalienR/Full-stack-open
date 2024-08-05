642fd4bbea8e0870696b06c77a17a244531d99ed
vonSchalienR <vonschalienrosa@gmail.com>
2024-08-05 14:10:52 +0300

    .

---
+++ part5/bloglist-frontend/src/components/bloglist.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Blog from './blog'
+
+const BlogList = ({
+  blogs,
+  handleLike,
+  handleBlogDelete,
+  loggedUser
+}) => (
+  <div>
+    {blogs.map(blog => (
+      <Blog
+        key={blog.id}
+        blog={blog}
+        handleLike={() => handleLike(blog)}
+        handleBlogDelete={() => handleBlogDelete(blog)}
+        loggedUser={loggedUser}
+      />
+    ))}
+  </div>
+)
+
+BlogList.propTypes = {
+  blogs: PropTypes.array.isRequired,
+  handleLike: PropTypes.func.isRequired,
+  handleBlogDelete: PropTypes.func.isRequired,
+  loggedUser: PropTypes.string.isRequired
+}
+
+export default BlogList
