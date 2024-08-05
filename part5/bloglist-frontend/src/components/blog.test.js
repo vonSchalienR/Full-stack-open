6b87ec4fa4e3573e44f3afeacee8b3d6adedd384
vonSchalienR <vonschalienrosa@gmail.com>
2024-08-05 14:13:04 +0300

    Update blog.test.js

--- part5/bloglist-frontend/src/components/blog.test.js
+++ part5/bloglist-frontend/src/components/blog.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
-import Blog from './Blog'
+import Blog from './blog'
 
 test('Blog renders the blog\'s title and author, but does not render its url or number of likes by default.', () => {
   const blog = {
