8ac6c6e184160096a459889cc979bafb60323183
vonSchalienR <vonschalienrosa@gmail.com>
2024-08-07 11:38:24 +0300

    .

---
+++ part7/bloglist-frontend/src/components/bloglist.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+import Table from 'react-bootstrap/Table'
+
+import Blog from './blog'
+
+const BlogList = () => {
+  const blogs = useSelector((state) => state.blogs)
+  const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes)
+
+  return (
+    <Table striped bordered hover>
+      <tbody>
+        {sortedBlogs.map((blog) => (
+          <tr key={blog.id}>
+            <td>
+              <Blog blog={blog} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  )
+}
+
+export default BlogList
---
+++ part7/bloglist-frontend/src/components/user.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import Blog from './blog'
+
+import Table from 'react-bootstrap/Table'
+
+const User = ({ user }) => {
+  if (!user) return null
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <h3>Added blogs</h3>
+      <Table striped bordered hover>
+        <tbody>
+          {user.blogs.map((blog) => (
+            <tr key={blog.id}>
+              <td>
+                <Blog blog={blog} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  )
+}
+
+export default User
